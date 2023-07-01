68ad1d1f2e89d3ef5b37435ea73280c2dd4cc6b9
SAHIL DAFADE <90718067+Sahil089@users.noreply.github.com>
2023-07-02 01:29:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, request, jsonify
 import pickle
 import re
 import nltk
-nltk.data.path.append('/home/sbx_user1051/nltk_data')
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
